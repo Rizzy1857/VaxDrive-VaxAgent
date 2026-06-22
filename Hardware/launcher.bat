a662b3f7b2acc4e1a7775ad0a46228f629c94e21
Rizzy <hrisheekeshpv@gmail.com>
2026-06-22 15:44:39 +0530

    Update launcher.bat

--- Hardware/launcher.bat
+++ Hardware/launcher.bat
@@ -2,7 +2,10 @@
 :: VaxDrive Target Payload Launcher
 :: This script is invoked by the ATtiny85 HID payload once the .vaxdrive marker is found.
 
+:: Provide fallback hardware token provider for testing
+set VAXDRIVE_HARDWARE_TOKEN_PROVIDER=MOCK
+
 :: Seamlessly execute the VaxAgent binary in the background, relative to this script's path
-start "" "%~dp0VaxAgent.exe"
+start "" "%~dp0VaxAgent.exe" --daemon
 
 exit
